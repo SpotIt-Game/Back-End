907e9f89066e44bdf95492885d8cfa3e9cfad0c5
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-02-15 16:29:33 -0600

    Update libs.h

--- code/libs.h
+++ code/libs.h
@@ -8,5 +8,44 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+
+
+#define SET(msk, i) ((msk)|(1<<(i)))
+#define TEST(msk, i) ((msk)&(1<<(i)))
+#define CLEAR(msk, i) ((msk)&~(1<<(i)))
+#define FLIP(mks, i) ((msk)^(1<<(i)))
+
+
+
+
+
+
+
+int factorial(int n){
+    if(!n) return 1;
+    return n * factorial(n-1);
+}
+
+
+
+int binomialCoefficient(int n, int k){
+    return factorial(n)/(factorial(n-k)*factorial(k));
+}
+
+
+
+
+int Pow(int b, int e){
+    if(!e) return 1;
+    if(e&1) return b*pow(b*b, e>>1);
+    return pow(b*b, e>>1);
+}
+
+
+
+
+
+
+
 #endif 
 
