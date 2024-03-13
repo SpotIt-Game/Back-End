608a17c6094cede2d46d9655b3ba818f89cba174
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-03-13 12:01:17 -0600

    Add files via upload

--- code/PNG.java
+++ code/PNG.java
@@ -9,28 +9,29 @@ public class PNG{
     private int[][] points = new int[4][2];
     private double scale, rotate;
     private Random random = new Random();
+    private double size = 2e5;
+
 
 
     public PNG(int id, int cols, int rows){
 
         id_image = id;
-        points[0][0] = 0;    points[0][1] = 0;
-        points[1][0] = cols; points[1][1] = 0;
-        points[2][0] = 0;    points[2][1] = rows;
-        points[3][0] = cols; points[3][1] = rows;
+        scale = Math.sqrt(size/(cols*rows) * (0.5 + (1.5 * random.nextDouble())));
 
-        scale = Math.sqrt(0.5 + (1.5 * random.nextDouble()));
+        points[0][0] = 0;                    points[0][1] = 0;
+        points[1][0] = (int)(cols * scale);  points[1][1] = 0;
+        points[2][0] = 0;                    points[2][1] = (int)(rows * scale);
+        points[3][0] = (int)(cols * scale);  points[3][1] = (int)(rows * scale);
+
+
         rotate = 2 * Math.PI * random.nextDouble();
-
-        int cX = cols >> 1, cY = rows >> 1;
+        int cX = points[3][0]>>1, cY = points[3][1]>>1;
         double cosTheta = Math.cos(rotate), sinTheta = Math.sin(rotate);
 
         for (int i = 0; i < 4; ++i) {
             int x = points[i][0] - cX, y = points[i][1] - cY;
-            points[i][0] = (int) (x * cosTheta - y * sinTheta) + cX;
-            points[i][1] = (int) (x * sinTheta + y * cosTheta) + cY;
-            points[i][0] *= scale;
-            points[i][1] *= scale;
+            points[i][0] = (int)(x * cosTheta - y * sinTheta) + cX;
+            points[i][1] = (int)(x * sinTheta + y * cosTheta) + cY;
         }
 
     }
@@ -47,6 +48,16 @@ public class PNG{
             points[2][0] + " " + points[2][1] + "\n" +
             points[3][0] + " " + points[3][1] + "\n" +
             rotate + "\n" + scale + "\n";
+
+    }
+
+
+
+
+    public static void main(String[] args){
+
+        PNG p = new PNG(1, 2440, 1000);
+        System.out.println(p);
 
     }
 
