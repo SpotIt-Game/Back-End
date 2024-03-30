2f7493e0a067fdb63b16538cd8ea54e02ccf206d
god-usoop <145166472+god-usoop@users.noreply.github.com>
2024-03-30 00:59:49 -0600

    Update ThePoisonedGift.java

--- code/Controller/ThePoisonedGift.java
+++ code/Controller/ThePoisonedGift.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 
 public class ThePoisonedGift{
-    private int gameMode =  5; //creo que esto ya no haría falta almenos para esta clase
+    // private int gameMode =  5; //creo que esto ya no haría falta almenos para esta clase
 
-        public int getgameMode(){
-            return this.gameMode;
-        }
+    //     public int getgameMode(){
+    //         return this.gameMode;
+    //     }
 
         @Override
         public void handleChange(Player p){ 
