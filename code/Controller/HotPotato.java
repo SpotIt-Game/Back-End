f9d81671f60275fedbc8f67bf1371179ce4eb8d9
god-usoop <145166472+god-usoop@users.noreply.github.com>
2024-03-29 23:29:32 -0600

    Update HotPotato.java

--- code/Controller/HotPotato.java
+++ code/Controller/HotPotato.java
@@ -1,5 +1,5 @@
 public class HotPotato {
-    private int gameMode =  2;
+    private int gameMode =  2; //creo que esto ya no haría falta almenos para esta clase
 
         public int getgameMode(){
             return this.gameMode;
