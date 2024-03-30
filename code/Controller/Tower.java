e9810ae010593830a648cae638f548ad16b7a8d9
god-usoop <145166472+god-usoop@users.noreply.github.com>
2024-03-29 23:30:38 -0600

    Update Tower.java

--- code/Controller/Tower.java
+++ code/Controller/Tower.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Tower {
-    private int gameMode =  1; 
+    private int gameMode =  1; //creo que esto ya no haría falta almenos para esta clase
 
     public int getgameMode(){
         return this.gameMode;
