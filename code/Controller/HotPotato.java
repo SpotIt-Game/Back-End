12f1bde8aea659668351066d88c7154192fe373c
god-usoop <145166472+god-usoop@users.noreply.github.com>
2024-03-30 00:36:46 -0600

    Update HotPotato.java

--- code/Controller/HotPotato.java
+++ code/Controller/HotPotato.java
@@ -1,8 +1,14 @@
-public class HotPotato {
-    private int gameMode =  2; //creo que esto ya no haría falta almenos para esta clase
+public class HotPotato extends Game{
+    // private int gameMode =  2; //creo que esto ya no haría falta almenos para esta clase
 
-        public int getgameMode(){
-            return this.gameMode;
-        }
+    //     public int getgameMode(){
+    //         return this.gameMode;
+    //     }
+
+    public HotPotato(ArrayList<Player>players){
+        super();
+    }
+
+        
 }
 
