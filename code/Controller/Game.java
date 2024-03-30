28b61100d075de40573df754008102c5c03a29ff
god-usoop <145166472+god-usoop@users.noreply.github.com>
2024-03-29 22:45:45 -0600

    Update Game.java

--- code/Controller/Game.java
+++ code/Controller/Game.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
+// public class Game implements Result{
+
+//     private int level, game_mode;
+//     private ArrayList<Player> players = new ArrayList<>();
+
+//     public Game(int level, int game_mode, ArrayList<Player> players){
+
+//         this.level = level;
+//         this.game_mode = game_mode;
+//         this.players = players;
+
+//     }
+//     // metodo que dirija el game_mode seleccionado
+// }
 
 
+public abstract class Game{
 
-
-public class Game implements Result{
-
-    private int level, game_mode;
-    private ArrayList<Player> players = new ArrayList<>();
-
-    public Game(int level, int game_mode, ArrayList<Player> players){
-
-        this.level = level;
-        this.game_mode = game_mode;
-        this.players = players;
-
-    }
-    // metodo que dirija el game_mode seleccionado
-
-
-
-
+    
+    
 }
