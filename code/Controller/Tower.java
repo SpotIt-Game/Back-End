280034b39d188efc3acfe9b773bcc16aa599210a
god-usoop <145166472+god-usoop@users.noreply.github.com>
2024-03-30 01:00:21 -0600

    Update Tower.java

--- code/Controller/Tower.java
+++ code/Controller/Tower.java
@@ -1,9 +1,14 @@
 import java.util.*;
 
 public class Tower {
-    private int gameMode =  1; //creo que esto ya no haría falta almenos para esta clase
+    // private int gameMode =  1; //creo que esto ya no haría falta almenos para esta clase
 
-    public int getgameMode(){
-        return this.gameMode;
+    // public int getgameMode(){
+    //     return this.gameMode;
+    // }
+
+    @Override
+    public void handleChange(Player p){ 
+        return;
     }
 }
